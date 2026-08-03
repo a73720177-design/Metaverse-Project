932d77578c725b25b71a97d4c88ac65370b4c697
imjae <a73720177@gmail.com>
2026-08-03 14:25:47 +0900

    Initial commit

---
+++ src/main/java/com/meta/git_test_12/GitTest12Application.java
@@ -0,0 +1,13 @@
+package com.meta.git_test_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTest12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/meta/git_test_12/GitTest12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.git_test_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
