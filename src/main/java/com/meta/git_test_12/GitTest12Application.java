a9b4dccf6ddd60ed205568d20f2e784d5c224e28
CYCL <6042ffg@gmail.com>
2026-08-03 15:01:15 +0900

    feat: 반갑다는 말 추가 35년이라는 상황 추가

--- src/main/java/com/meta/git_test_12/GitTest12Application.java
+++ src/main/java/com/meta/git_test_12/GitTest12Application.java
@@ -8,6 +8,8 @@ public class GitTest12Application {
 
     public static void main(String[] args) {
         SpringApplication.run(GitTest12Application.class, args);
+        System.out.println("너무 반갑다. 35년 만인가?");
     }
+
 
 }
