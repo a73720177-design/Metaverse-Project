6ae00adebe08eccdcac2d62f84ff83a55118799c
lovelovelyst0419-source <lovelovelyst0419@gmail.com>
2026-08-07 00:31:15 +0900

    Update GitTest12Application.java
    김승태그는감히전설이라할수있다

--- src/main/java/com/meta/git_test_12/GitTest12Application.java
+++ src/main/java/com/meta/git_test_12/GitTest12Application.java
@@ -11,6 +11,7 @@ public class GitTest12Application {
         System.out.println("너무 반갑다. 35년 만인가?");
       System.out.printf(" 안녕하세요");  
       System.out.println("브랜치 생성 완료");
+        system.out.println("내가 돌아왔다.");
     }
 
 
